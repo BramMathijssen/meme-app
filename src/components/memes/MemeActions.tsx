336dca65521aeb21aabe2ce630250c7b421acebb
Bram Mathijssen <Bram.mathijssen@gmail.com>
2022-10-11 00:27:52 +0200

    added framer motion

--- src/components/memes/MemeActions.tsx
+++ src/components/memes/MemeActions.tsx
@@ -2,14 +2,27 @@ import { FolderPlus } from "phosphor-react";
 import React from "react";
 import "./MemeActions.scss";
 import { ArrowUp, ArrowDown, ChatText } from "phosphor-react";
+import { motion } from "framer-motion";
 
 const MemeActions = () => {
   return (
     <div className="actions">
       <div className="actions__controls">
-        <ArrowUp color="#32ec76" size={25} />
+        <motion.a
+          whileHover={{
+            scale: 1.2,
+          }}
+        >
+          <ArrowUp color="#32ec76" size={25} />
+        </motion.a>
         <p>0</p>
-        <ArrowDown color="#32ec76" size={25} />
+        <motion.a
+          whileHover={{
+            scale: 1.2,
+          }}
+        >
+          <ArrowDown color="#32ec76" size={25} />
+        </motion.a>
       </div>
       <div className="actions__comments">
         <p>0</p>
